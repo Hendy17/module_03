0eb245f4bfe846a8811c83c225c36025695e4417
Hendy17 <hendyvorpagel@gmail.com>
2024-08-23 20:06:39 -0300

    Update Roles

--- week_01/api_escola/src/routes/rbac.route.js
+++ week_01/api_escola/src/routes/rbac.route.js
@@ -6,5 +6,6 @@ const RbacController = new Rauter()
 RbacRouter.get("/listPermission, RbacController.lisPermissions")
 RbacRouter.get("/listRole, RbacController.lisRoles")
 RbacRouter.post("/createOnePermission, RbacController.createPermission")
+RbacRouter.post("/createOneRole, RbacController.createRole")
 
 module.exports = RbacRouter
